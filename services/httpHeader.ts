a1e89d443527d514c4a21e6a6969f0409e714879
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-06-25 18:20:11 +0200

    add endpoint

--- services/httpHeader.ts
+++ services/httpHeader.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    "https://eu-central-1.aws.data.mongodb-api.com/app/application-0-zuxni/endpoint/",
+    "https://eu-central-1.aws.data.mongodb-api.com/app/smoking-backend-ftcyvvz/endpoint/",
   headers: {
     "Content-Type": "application/json",
   },
